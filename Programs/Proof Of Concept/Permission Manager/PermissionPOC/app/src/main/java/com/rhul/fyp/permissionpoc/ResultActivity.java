1035208ffe25e47d37165a71f72648d5e4bf0b7e
Mohamed Hassan Abdou M Soliman Abd (2020) ZJAC078 <zjac078>
2022-11-06 18:08:06 +0000

    Created Activity to actually show all apps in a clickable list after scan.

---
+++ Programs/Proof Of Concept/Permission Manager/PermissionPOC/app/src/main/java/com/rhul/fyp/permissionpoc/ResultActivity.java
@@ -0,0 +1,71 @@
+package com.rhul.fyp.permissionpoc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NavUtils;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ResultActivity extends AppCompatActivity {
+    RecyclerView appCardList;
+    RecyclerView.LayoutManager layout;
+    AppAdapter adapter;
+    public static ArrayList<AppParcelInfo> apps;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.permissions_result);
+        appCardList = findViewById(R.id.resultList);
+
+        layout = new LinearLayoutManager(this);
+        appCardList.setLayoutManager(layout);
+
+        adapter = new AppAdapter(this, apps);
+        appCardList.setAdapter(adapter);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
