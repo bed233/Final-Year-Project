7268596a1857fe50e8ad7bc24689bae66d59fe38
Mohamed Hassan Abdou M Soliman Abd (2020) ZJAC078 <zjac078>
2022-11-06 19:00:40 +0000

    Added ability to interact with each permission and show a pop up when user clicks permission by borrowing code from libreAV.

--- Programs/Proof Of Concept/Permission Manager/PermissionPOC/app/src/main/java/com/rhul/fyp/permissionpoc/PermissionListAdapter.java
+++ Programs/Proof Of Concept/Permission Manager/PermissionPOC/app/src/main/java/com/rhul/fyp/permissionpoc/PermissionListAdapter.java
@@ -1,6 +1,9 @@
 package com.rhul.fyp.permissionpoc;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionInfo;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +18,9 @@ public class PermissionListAdapter extends RecyclerView.Adapter<PermissionListAd
     private Context context;
     private ArrayList<String> arrayList;
 
-    public PermissionListAdapter(Context context, ArrayList<String> arrayList){
-        this.arrayList=arrayList;
-        this.context=context;
+    public PermissionListAdapter(Context context, ArrayList<String> arrayList) {
+        this.arrayList = arrayList;
+        this.context = context;
     }
 
     @NonNull
@@ -36,22 +39,68 @@ public class PermissionListAdapter extends RecyclerView.Adapter<PermissionListAd
 
     @Override
     public int getItemCount() {
-        if(arrayList.isEmpty()){
+        if (arrayList.isEmpty()) {
             return 0;
-        } else{
+        } else {
             return arrayList.size();
         }
     }
 
-    class ViewHolder extends RecyclerView.ViewHolder{
+    class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView name;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            this.name=itemView.findViewById(R.id.name);
+            this.name = itemView.findViewById(R.id.name);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    PermissionDialog permissionDialog = new PermissionDialog();
+                    permissionDialog.showDialog(context, arrayList.get(getAdapterPosition()));
+
+                }
+            });
         }
     }
+}
 
+/**
+ * Allows each permission to be clicked in the list where a pop up will appear giving user a bit
+ * more information regarding the permission.
+ * <p>
+ * Borrowed from: https://github.com/projectmatris/antimalwareapp/blob/development/app/src/main/java/tech/projectmatris/antimalwareapp/adapters/ScanResultAdapter.java
+ * </p>
+ */
+class PermissionDialog {
+    public void showDialog(Context context, String permission) {
+        PackageManager packageManager = context.getPackageManager();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        CharSequence title = null;
+        CharSequence desc;
+        try {
+            PermissionInfo permissionInfo = packageManager.getPermissionInfo(permission, PackageManager.GET_META_DATA);
+            if (permissionInfo.group != null) {
+                title = packageManager.getPermissionGroupInfo(permissionInfo.group, 0).loadLabel(packageManager);
+                builder.setIcon(packageManager.getPermissionGroupInfo(permissionInfo.group, 0).loadIcon(packageManager));
+            }
+            desc = permissionInfo.loadDescription(packageManager);
+            if (desc == null) {
+                desc = permissionInfo.nonLocalizedDescription;
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            title = context.getString(R.string.permission_info);
+            desc = context.getString(R.string.no_description_found);
+        }
+        if (title == null) {
+            title = context.getString(R.string.permission_info);
+        }
+        if (desc == null) {
+            desc = context.getString(R.string.no_description_found);
+        }
+        builder.setMessage(desc);
+        builder.setTitle(title);
+        builder.create().show();
 
+    }
 }
