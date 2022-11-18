92a880e925ada66e1fb225e3f0fa41061c81c5c9
Mohamed Hassan Abdou M Soliman Abd (2020) ZJAC078 <zjac078>
2022-11-18 18:47:31 +0000

    Created Pattern Class that will communicate with main class to get info about pattern and update status.

---
+++ Programs/Proof Of Concept/Permission Manager/AppLockPOC/app/src/main/java/com/rhul/fyp/applockpoc/model/pattern.java
@@ -0,0 +1,40 @@
+package com.rhul.fyp.applockpoc.model;
+
+import android.content.Context;
+
+import io.paperdb.Paper;
+
+public class pattern {
+    private String patternKey = "Pattern Key";
+    public String patternSet = "Pattern Set";
+    public String confirmPattern = "Draw the Pattern again to confirm";
+    public String patternIncorrect = "Please Try Again!";
+    public String firstPatternInput = "Draw an unlock pattern to unlock app";
+    public String patternFailed = "Pattern must connect at least 4 dots";
+    public Boolean firstAttempt = true;
+
+    public String getPatternKey() {
+        return Paper.book().read(patternKey);
+    }
+
+    public void setPatternKey(String pattern) {
+        Paper.book().write(patternKey, pattern);
+    }
+
+    public Boolean getFirstAttempt() {
+        return firstAttempt;
+    }
+
+    public void setFirstAttempt(Boolean firstAttempt) {
+        this.firstAttempt = firstAttempt;
+    }
+
+    public boolean patternIsCorrect(String pattern){
+        return pattern.equals(getPatternKey());
+    }
+
+    public pattern(Context context){
+        Paper.init(context);
+
+    }
+}
