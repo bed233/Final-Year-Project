c9ac12aa18b24308c1899af783054bce9f596078
Mohamed Hassan Abdou M Soliman Abd (2020) ZJAC078 <zjac078>
2022-12-02 01:48:58 +0000

    Linking Permission Manager button to Permission Manager and adding intent in manifest file.

--- Programs/Android Security Suite/Interim/app/src/main/java/com/rhul/fyp/interimsuite/MainActivity.java
+++ Programs/Android Security Suite/Interim/app/src/main/java/com/rhul/fyp/interimsuite/MainActivity.java
@@ -2,10 +2,13 @@ package com.rhul.fyp.interimsuite;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+
+import com.rhul.fyp.interimsuite.permissionpoc.PermissionsScanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,15 +19,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Button fileScan = findViewById(R.id.fileScan);
         final Button appScan = findViewById(R.id.appScan);
-        fileScan.setOnClickListener(view ->{
+        final Button permissionManager = findViewById(R.id.permissionManager);
+        fileScan.setOnClickListener(view -> {
             finish();
             startActivity(new Intent(com.rhul.fyp.interimsuite.MainActivity.this,
                     com.rhul.fyp.interimsuite.hypatia.MainActivity.class));
         });
-        appScan.setOnClickListener(view ->{
+        appScan.setOnClickListener(view -> {
             finish();
             startActivity(new Intent(com.rhul.fyp.interimsuite.MainActivity.this,
                     com.rhul.fyp.interimsuite.libreav.activities.MainActivity.class));
+        });
+        PermissionsScanner scan = new PermissionsScanner(this, MainActivity.this);
+        permissionManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scan.execute();
+            }
         });
     }
 }
