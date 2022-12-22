9473fda347d84657016edc1a4818b67805a0c89c
Mohamed Hassan Abdou M Soliman Abd (2020) ZJAC078 <zjac078>
2022-12-22 18:55:41 +0000

    uploading AppLock POC and Interim Submission

---
+++ Interim Submission/AppLockPOC/app/src/main/java/de/Maxr1998/xposed/maxlock/ui/settings_new/HiddenSettingsReceiver.kt
@@ -0,0 +1,28 @@
+/*
+ * MaxLock, an Xposed applock module for Android
+ * Copyright (C) 2014-2018  Max Rumpf alias Maxr1998
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.Maxr1998.xposed.maxlock.ui.settings_new
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class HiddenSettingsReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        context.startActivity(Intent(context, SettingsActivity::class.java).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
+    }
+}
