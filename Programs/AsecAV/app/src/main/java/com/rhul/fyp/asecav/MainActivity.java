29f785326f5874b0853fd4859e86ad7a7cfa0095
zjac078 <zjac078@live.rhul.ac.uk>
2023-02-02 15:42:10 +0000

    Added delay in case scanner has not finished in order to prevent app from crashing

--- Programs/AsecAV/app/src/main/java/com/rhul/fyp/asecav/MainActivity.java
+++ Programs/AsecAV/app/src/main/java/com/rhul/fyp/asecav/MainActivity.java
@@ -2,6 +2,7 @@ package com.rhul.fyp.asecav;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import com.rhul.fyp.asecav.permissionpoc.PermissionsScanner;
+import com.rhul.fyp.asecav.permissionpoc.ResultActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         final Button appScan = findViewById(R.id.appScan);
         final Button permissionManager = findViewById(R.id.permissionManager);
         final Button maxLock = findViewById(R.id.appLocker);
+        PermissionsScanner scan = new PermissionsScanner(this, MainActivity.this);
+        scan.execute();
         maxLock.setOnClickListener(view -> {
             finish();
             startActivity(new Intent(com.rhul.fyp.asecav.MainActivity.this,
@@ -36,12 +40,28 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(com.rhul.fyp.asecav.MainActivity.this,
                     com.rhul.fyp.asecav.libreav.activities.MainActivity.class));
         });
-        PermissionsScanner scan = new PermissionsScanner(this, MainActivity.this);
-        permissionManager.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                scan.execute();
+        permissionManager.setOnClickListener(view -> {
+            if (!scan.finished){
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        finish();
+                        Intent resultScreen = new Intent(scan.contextRef.get(), ResultActivity.class);
+                        scan.contextRef.get().startActivity(resultScreen);                }
+                }, 600);
+            }else{
+                finish();
+                Intent resultScreen = new Intent(scan.contextRef.get(), ResultActivity.class);
+                scan.contextRef.get().startActivity(resultScreen);
             }
+
         });
+//        PermissionsScanner scan = new PermissionsScanner(this, MainActivity.this);
+//        permissionManager.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                scan.execute();
+//            }
+//        });
     }
 }
