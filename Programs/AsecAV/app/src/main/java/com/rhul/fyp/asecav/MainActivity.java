532cbff64da1d48b89e6ce982e605fcf9a86047c
zjac078 <zjac078@live.rhul.ac.uk>
2023-03-08 01:04:04 +0000

    Sending values to the Storage Widget to output to the user.

--- Programs/AsecAV/app/src/main/java/com/rhul/fyp/asecav/MainActivity.java
+++ Programs/AsecAV/app/src/main/java/com/rhul/fyp/asecav/MainActivity.java
@@ -2,13 +2,17 @@ package com.rhul.fyp.asecav;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.os.StatFs;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import com.ramijemli.percentagechartview.PercentageChartView;
 import com.rhul.fyp.asecav.permissionpoc.PermissionsScanner;
 import com.rhul.fyp.asecav.permissionpoc.ResultActivity;
 
@@ -19,6 +23,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
+        final StatFs stats = new StatFs(Environment.getExternalStorageDirectory().getPath());
+        final long gigabyteAvailable =
+                (stats.getBlockSizeLong() * stats.getAvailableBlocksLong()) / (1024 * 1024 * 1024);
+        final long totalSpace =
+                (stats.getBlockSizeLong() * stats.getBlockCountLong()) / (1024 * 1024 * 1024) ;
+        final long gigabyteUsed = totalSpace - gigabyteAvailable;
+        final float percentUsed = (float)((float)gigabyteUsed / (float)totalSpace) * 100;
+        final PercentageChartView storageStats = findViewById(R.id.storage_percent);
+        storageStats.setProgress(percentUsed, true);
+        final TextView freeSpace = findViewById(R.id.free_space);
+        final TextView usedSpace = findViewById(R.id.used_space);
+        final TextView totalSpaceText = findViewById(R.id.total_space);
+        totalSpaceText.setText((int) totalSpace + "GB");
+        freeSpace.setText((int) gigabyteAvailable + "GB");
+        usedSpace.setText((int) gigabyteUsed + "GB");
         final Button fileScan = findViewById(R.id.fileScan);
         final Button appScan = findViewById(R.id.appScan);
         final Button permissionManager = findViewById(R.id.permissionManager);
